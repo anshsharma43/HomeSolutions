d63c10cd0938b269f37d853aaf55ac2aca879ff4
Ansh <anshsharma.niku@gmail.com>
2022-07-12 21:31:49 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Home Solutions
+
+
+## Information
+
+#### What is used?
+- PHP7, MySQL
+- HTML, Bootstrap(Theme is from [Bootswatch](https://bootswatch.com)), Jquery
+
+#### Login Info
+- Admin:
+  - Mobile No.: 7070808080
+  - Password  : admin123
+- Providers:
+    - No Providers are registered by default use register screen to create
+      some.
+
+
+## Features
+- Search for professionals for plumbing, mobile repaires, etc. in your city.
+- Register as service provider. Update providers information and password
+  through.
+  Providers Login.
+- Admin can see and manage bookings.
+- Admin can see and manage providers.
---
+++ about.php
@@ -0,0 +1,28 @@
+<?php include_once "./include/header.php"; ?>
+
+<div class="container">
+    <div class="row">
+        <img src="images/homesolutions.png" class="col-4 card-img-top" alt="Logo">
+
+        <div class="container col-8" style="margin-top: 50px;">
+            <div class="card">
+                <div class="card-header text-center">
+                    <strong>Home Solutions</strong>
+                </div>
+
+                <div class="card-body">
+
+                <strong class="card-title">Project By:</strong> <br><br>
+                <p>Ansh Sharma</p>
+            </div>
+
+            <div class="card-footer text-center">
+                A dynamic web application intended to cater to the needs of people seeking home services
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php include_once "./include/footer.php"; ?>
